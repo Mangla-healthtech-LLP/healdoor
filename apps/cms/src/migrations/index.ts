c66418cd024ed562ad2c0b3958d881df1d8065ba
Vani Rao <vanirao322@gmail.com>
2026-06-13 20:28:14 +0530

    project setup

---
+++ apps/cms/src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260613_141252 from './20260613_141252';
+
+export const migrations = [
+  {
+    up: migration_20260613_141252.up,
+    down: migration_20260613_141252.down,
+    name: '20260613_141252'
+  },
+];
